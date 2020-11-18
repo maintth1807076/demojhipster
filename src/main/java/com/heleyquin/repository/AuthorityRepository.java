5c6d62e0b88d86d48275ec4653a00147ee8da010
mai.ngo.intern <mai.ngo.intern@ntq-solution.com.vn>
2020-11-19 06:04:15 +0700

    Initial version of demojhipster generated by JHipster-6.10.5

---
+++ src/main/java/com/heleyquin/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.heleyquin.repository;
+
+import com.heleyquin.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/heleyquin/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.heleyquin.web.rest.vm;
